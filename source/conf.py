bc7f83db4489ed32fb0ad2e01d3372258765e742
C.A.M. Gerlach <widenetservices@gmail.com>
2018-09-22 22:56:29 -0500

    MIN: Convert date reprisentation to international ISO 8601 format

--- source/conf.py
+++ source/conf.py
@@ -138,7 +138,7 @@ print("RELEASE, VERSION", release, version)
 # non-false value, then it is used:
 # today = ''
 # Else, today_fmt is used as the format for a strftime call.
-today_fmt = '%B %d, %Y'
+today_fmt = '%Y-%m-%d'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
